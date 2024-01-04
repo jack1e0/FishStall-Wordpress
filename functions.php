d58ee66dd4a981fd0d1ad0fc645631044c04dec6
Jackie <jackieguo126@gmail.com>
2024-01-04 15:50:57 +0800

    Cleaning

--- functions.php
+++ functions.php
@@ -6,9 +6,4 @@ function homepagetheme_parent_styles() {
  wp_enqueue_style( 'parent', get_template_directory_uri().'/style.css' );                       
 }
 
-// function enqueue_custom_script() {
-//     wp_enqueue_script('main', get_template_directory_uri() . '/main.js', array('jquery'), '1.0', true);
-// }
-// add_action('wp_enqueue_scripts', 'enqueue_custom_script');
-
 add_action( 'wp_enqueue_scripts', 'homepagetheme_parent_styles');
